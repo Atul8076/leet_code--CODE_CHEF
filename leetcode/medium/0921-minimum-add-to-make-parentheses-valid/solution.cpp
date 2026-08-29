0f0c9b49f6cfa49f124bc7d6d9f4fd782da90f90
Atul8076 <atuly8076@gmail.com>
2026-08-29 22:39:54 +0530

    ✅ Minimum Add to Make Parentheses Valid | Medium | C++ | Runtime: 0 ms (100.00%) | Memory: 8.5 MB | 2026-08-29

--- leetcode/medium/0921-minimum-add-to-make-parentheses-valid/solution.cpp
+++ leetcode/medium/0921-minimum-add-to-make-parentheses-valid/solution.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-      stack<char>st;
-      for(int i = 0; i<s.length() ; i++){
-        if(st.empty()){
-            st.push(s[i]);
+        stack<char>st;
+        for(int i = 0 ; i<s.length(); i++){
+            if(s[i] == '('){
+                st.push(s[i]);
+            }
+            else{
+                if(!st.empty() && st.top() == '('){
+                    st.pop();
 
+                }
+                else{
+                    st.push(s[i]);
+                }
+            }
         }
-        else if(s[i] == st.top()){
-                st.pop();
-        }
-        else{
-            st.push(s[i]);
-        }
-      }
-      return st.size() ; 
+        return st.size();
     }
 };
