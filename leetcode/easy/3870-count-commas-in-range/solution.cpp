8e02a826502f503e88c063d1d43dca90e97e50ba
Atul8076 <atuly8076@gmail.com>
2026-09-09 00:32:47 +0530

    ✅ Count Commas in Range | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 8.5 MB | 2026-09-08

---
+++ leetcode/easy/3870-count-commas-in-range/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        long long totalCommas = 0;
+        
+        if (n >= 1000) {
+            totalCommas += (n - 1000 + 1);
+        }
+        if (n >= 1000000) {
+            totalCommas += (n - 1000000 + 1);
+        }
+        if (n >= 1000000000) {
+            totalCommas += (n - 1000000000 + 1);
+        }
+        
+        return totalCommas;
+    }
+};
